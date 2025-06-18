5bfedcd860c0dc5663d4071eabe20f72052f0029
Juno Zhang <nightstraps@gmail.com>
2025-06-18 19:38:41 -0400

    🤫

--- game-stash/src/app/app.component.ts
+++ game-stash/src/app/app.component.ts
@@ -16,17 +16,21 @@ export class AppComponent {
     this.fetchMessage();
   }
 
-  async fetchMessage() {
-    try {
-      const response = await fetch('http://localhost:3000/');
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      this.message = data.message;
-    } catch (error) {
-      this.message = 'Error fetching data';
-      console.error(error);
-    }
+  fetchMessage() {
+    fetch('http://localhost:3000/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.message = data.message;
+      })
+      .catch(error => {
+        this.message = 'Error fetching data';
+        console.error(error);
+      });
   }
+
 }
