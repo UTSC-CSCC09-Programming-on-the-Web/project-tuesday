c3260ff4f199736f705a6709d28959f972018ba7
JeremyNeilson <jeremyneilson1@gmail.com>
2025-06-19 20:30:31 -0600

    feat: added create lobby component

---
+++ game-stash/src/app/desk-create-lobby/desk-create-lobby/desk-create-lobby.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeskCreateLobbyComponent } from './desk-create-lobby.component';
+
+describe('DeskCreateLobbyComponent', () => {
+  let component: DeskCreateLobbyComponent;
+  let fixture: ComponentFixture<DeskCreateLobbyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DeskCreateLobbyComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DeskCreateLobbyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
